cc555cc62c5e99f0939a80ee727cba51120c415c
Cha1nHeart <humanoverrated@gmail.com>
2023-04-26 11:37:29 +0500

    PosterManager added

---
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -0,0 +1,42 @@
+package ru.netology.manager;
+
+public class PosterManager {
+    private String[] posters = new String[0];
+    private int limit;
+
+    public PosterManager() {
+        this.limit = 5;
+    }
+
+    public PosterManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addFilm(String film) {
+        String[] tmp = new String[posters.length + 1];
+        for (int i = 0; i < posters.length; i++) {
+            tmp[i] = posters[i];
+        }
+        tmp[tmp.length - 1] = film;
+        posters = tmp;
+    }
+
+    public String[] findAll() {
+        return posters;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (posters.length < limit) {
+            resultLength = posters.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = posters[posters.length - 1 - i];
+        }
+        return tmp;
+    }
+
+}
---
+++ src/test/java/ru/netology/test/PosterManagerTest.java
@@ -0,0 +1,88 @@
+package ru.netology.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.netology.manager.PosterManager;
+
+public class PosterManagerTest {
+
+    PosterManager manager = new PosterManager();
+
+//    Добавление новых фильмов
+
+    @Test
+    public void shouldAddFilms() {
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
+        String[] actual = manager.findAll();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddOneFilm() {
+        manager.addFilm("Film 1");
+
+        String[] expected = {"Film 1"};
+        String[] actual = manager.findAll();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddNull() {
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+//    Последние добавленные фильмы, выведенные в обратном порядке.
+//    Лимит - пять.
+
+    @Test
+    public void shouldFindLastLimit() {
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+        manager.addFilm("Film 4");
+        manager.addFilm("Film 5");
+
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindLastAboveLimit() {
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+        manager.addFilm("Film 4");
+        manager.addFilm("Film 5");
+        manager.addFilm("Film 6");
+
+        String[] expected = {"Film 6", "Film 5", "Film 4", "Film 3", "Film 2"};
+        String[] actual = manager.findLast();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastBellowLimit() {
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+}
